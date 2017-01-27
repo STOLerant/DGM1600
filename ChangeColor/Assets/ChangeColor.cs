e42c60ed40e1db918310395ff3c64d6cd8853655
Unknown <Tswmartin@gmail.com>
2017-01-27 10:54:12 -0700

    ColorChange

---
+++ ChangeColor/Assets/ChangeColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+
+
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            print("Changed Color to Red."); }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+	}
+}

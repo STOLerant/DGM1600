5bc04519e795cd351f1f08cbde2818583c5c3bfb
Unknown <Tswmartin@gmail.com>
2017-02-03 10:41:45 -0700

    math
    added math

---
+++ MAth_op/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public int addition = 12 + 4;
+    public int subtraction = 12 - 4;
+    public float division = 5.0f / 2.0f;
+    public int multiplication = 10 * 2;
+    public int remainder = 7 % 2; 
+
+
+	// Use this for initialization
+	void Start () {
+        addition += 9;// += adds to
+        subtraction -= 10; // subtract from
+        addition += 3;
+        addition = 1;
+
+
+	}
+	
+}

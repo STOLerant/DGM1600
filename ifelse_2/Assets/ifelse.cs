f9544b85fa0482e9911cfa6b8e70064ce00e9452
Unknown <Tswmartin@gmail.com>
2017-02-08 10:42:35 -0700

    IfElse
    added IfElse

---
+++ ifelse_2/Assets/ifelse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifelse : MonoBehaviour {
+
+
+    public string password;
+    public bool UserAgreement;
+
+	// Use this for initialization
+	void Start () {
+
+           if(password == "OU812"  || UserAgreement)
+        {
+            print("Correct");
+        }
+           else
+        {
+            print("Incorrect");
+        }
+	}
+
+}

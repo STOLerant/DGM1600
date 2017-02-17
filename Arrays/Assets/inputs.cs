5271d56299d345e0c1bc9fa71cc6bc1aec52a7da
Unknown <Tswmartin@gmail.com>
2017-02-17 10:46:01 -0700

    inputs
    added inputs, some file names are "Arrays". oops.

---
+++ Arrays/Assets/inputs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboardinputs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))  //important infomation//
+        {
+            print("I hit A");
+       
+        }
+	}
+}
---
+++ Arrays/Assets/mouseinputs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseinputs : MonoBehaviour {
+
+    public void MyMouseClick ()
+    {
+        print("I clicked the button");
+    }
+
+}

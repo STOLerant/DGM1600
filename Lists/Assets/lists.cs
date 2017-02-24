30a22ddffcdc93110129d773e90a9c396f6b65c7
Unknown <Tswmartin@gmail.com>
2017-02-24 10:40:25 -0700

    problem2
    problem2

---
+++ Lists/Assets/lists.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+public class lists : MonoBehaviour {
+
+    public string myWeapon;
+    public List<string> myWeaponList;
+    public string[] myWeaponArray;
+
+
+	void Start () {
+        myWeaponList.Add("Sword");
+        //myWeaponList.Remove("Piece of Ham");
+        myWeaponList.Add("Bow");
+        //myWeaponList.RemoveAt(1);
+
+         
+		
+	}
+
+    public Void AddToList (myWeapon)
+    {
+        myWeaponList.Add("Rock");
+    }
+}

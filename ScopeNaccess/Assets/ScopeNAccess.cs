8b7083ce9ad9a378af439c5d3bcb3bddcce491de
Unknown <Tswmartin@gmail.com>
2017-03-06 10:53:10 -0700

    scopeNaccess
    forloops added too

---
+++ ScopeNaccess/Assets/ScopeNAccess.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeNAccess : MonoBehaviour {
+
+    public string owner = "Bob";
+    private string renter = "Kate";
+    public void Awake ()
+
+    {
+        string cat = "Joe";
+
+        if (cat == "Joe")
+        {
+            //print(renter);//
+            string mouse = "Helen";
+            print(cat + " ate " + mouse);
+        }
+        print(renter);
+        print(cat);
+        print(owner);
+    }
+
+    void Start ()
+    {
+        string cat = "Frank";
+        print(cat);
+        print(owner);
+	}
+}
---
+++ ScopeNaccess/Assets/access.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class access : MonoBehaviour {
+
+    public Scope home;
+	
+	// Update is called once per frame
+	void Start () {
+        print(home.owner);
+        //print(home.Awake());
+        home.Awake();
+    }
+}
